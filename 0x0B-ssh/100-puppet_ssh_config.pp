64c33dbe491071ec8f02adce2817582b98a8e0ae
LilyPr <lilyeginald09@gmil.com>
2023-01-10 21:05:18 +0300

    create ssh files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Sets up your client SSH configuration file to use a private key
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
