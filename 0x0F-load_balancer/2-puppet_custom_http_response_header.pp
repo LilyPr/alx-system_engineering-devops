f86f29f89b4317c7589d9ad0e6c1ee0b6d748bdd
LilyPr <lilyeginald09@gmil.com>
2023-01-24 15:16:29 +0300

    add

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,10 @@
-# Custom HTTP header response, so that it configures a brand new Ubuntu
-# machine to the requirements asked in this task
-exec { 'http':
-  command  => 'sudo apt-get update &&
-     	      sudo apt-get -y install nginx && var="add_header X-Served-By:
-	      $HOSTNAME" &&
-     	      sudo sed -i "50i$var" /etc/nginx/sites-available/default &&
-     	      sudo service nginx start',
-  provider => shell
-}
+# A Puppet script that configures the Nginx so that its HTTP response contains a custom header
+exec {'/usr/bin/env sudo apt-get update':}
+exec {'/usr/bin/env sudo apt-get -y install nginx':}
+exec {'/usr/bin/env ufw sudo allow "Nginx HTTP"':}
+exec {'/usr/bin/env echo "Hello World!" > /var/www/html/index.nginx-debian.html':}
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /usr/share/nginx/html/custom_404.html':}
+exec {'/usr/bin/env sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default':}
+exec {'/usr/bin/env sed -i "/listen 80 default_server/a error_page 404 /custom_404.html;location = /custom_404.html {root /usr/share/nginx/html;internal;}" /etc/nginx/sites-available/default':}
+exec {'/usr/bin/env sed -i "/listen 80 default_server/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default':}
+exec {'/usr/bin/env service nginx restart':}}
