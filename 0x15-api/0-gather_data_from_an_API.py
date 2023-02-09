a08de535fc6b242166ba116d3816864035e4cb6c
LilyPr <lilyeginald09@gmail.com>
2023-02-09 20:28:30 +0300

    api files

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """ Uses JSONPlaceholder API to get information about emplif __name__ == "__main__":
 """
-import json
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-        Re = requests.get('https://jsonplaceholder.typicode.com/users/{:}'
-                         .format(argv[1])).json()
-        Re_two = requests.get(
-                'https://jsonplaceholder.typicode.com/todos/?userId={:}'
-                .format(argv[1])).json()
-
-        EMPLOYEE_NAME = Re.get('name')
-        TASK_TITLE = []
-        NUMBER_OF_DONE_TASKS = 0
-
-        for task in Re_two:
-                if task.get('completed') is True:
-                        TASK_TITLE.append(task.get('title'))
-                        NUMBER_OF_DONE_TASKS += 1
-        TOTAL_NUMBER_OF_TASKS = len(Re_two)
-        print("Employee {} is done with tasks({}/{}):".
-              format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
-                     TOTAL_NUMBER_OF_TASKS))
-
-        for t in TASK_TITLE:
-                print("\t {}".format(t))
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
