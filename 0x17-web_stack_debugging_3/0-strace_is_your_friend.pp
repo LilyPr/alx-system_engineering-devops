e4be650742c47193b57d25b53b65c052c7d8b2fb
LilyPr <lilyeginald09@gmail.com>
2023-03-02 12:43:39 +0300

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,6 @@
-# Fixes the typo extension in wp-setting file
-exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }}
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
+exec {'replace':
+  provider => ubuntu,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
