1528ffe660d84329c4ebe9a3027e3ae6ce5c28df
LilyPr <lilyeginald09@gmail.com>
2023-03-02 12:09:12 +0300

    add api advanced files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ function that queries the Reddit API
+ and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """initializate"""
+    if (type(subreddit) is not str):
+        return(0)
+    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    headers= {"User-Agent": "custom"}
+    response = requests.get(url_api, headers=headers)
+    if response.status_code is not 200:
+        return(0)
+    return(response.json().get("data").get("subscribers"))
