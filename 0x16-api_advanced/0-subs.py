452c8b4d94d7325d1678c438ac4c5d7bbc864e13
LilyPr <lilyeginald09@gmail.com>
2023-03-02 12:13:17 +0300

    add api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
- function that queries the Reddit API
- and returns the number of subscribers
-"""
+''' get subs '''
 import requests
+
+url = 'https://www.reddit.com/r/{}/about.json'
 
 
 def number_of_subscribers(subreddit):
-    """initializate"""
-    if (type(subreddit) is not str):
-        return(0)
-    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
-    headers= {"User-Agent": "custom"}
-    response = requests.get(url_api, headers=headers)
-    if response.status_code is not 200:
-        return(0)
-    return(response.json().get("data").get("subscribers"))
+    '''get subscribers'''
+    header = {'user-agent': 'custom'}
+    r = requests.get(url.format(subreddit), headers=header)
+    if r.status_code != 200:
+        return 0
+    return r.json().get("data", {}).get("subscribers", 0)
